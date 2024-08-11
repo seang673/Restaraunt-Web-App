cf670a8df7e80f71ec153325b34200b0540d3a02
seang673 <seangriffiths673@gmail.com>
2024-08-11 17:34:09 -0400

    update appjs and new components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <h1>This is our header</h1>
-    </div>
+      <Header/>
+      <Nav/>
+      <Main/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const header = () => {
+    return (
+        <header>
+            <img src="Logo.370f832fad423c516d56.png"></img>
+        </header>
+    );
+
+};
+export default Header;
